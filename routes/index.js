fc154db3b462d7e73588c53bc05b0346b6e85298
nihitx <nihit@Nihits-MacBook-Pro.local>
2017-01-21 03:41:25 +0200

    changed title to see if new url works

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'IOT METROPOLIA' });
+  res.render('index', { title: 'IOT BACKEND METROPOLIA' });
 });
 
 module.exports = router;
